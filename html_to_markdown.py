e2446543e1e5125b2bb0cb468fffc5f929b399e4
veaba <908662421@qq.com>
2019-10-29 18:14:07 +0800

    update html to markdown code block

--- html_to_markdown.py
+++ html_to_markdown.py
@@ -1,3 +1,4 @@
+import re
 a='<table class="tfo-notebook-buttons tfo-api" align="left"><tbody><tr><td>  <a target="_blank" href="/versions/r1.15/api_docs/python/tf/clip_by_value">  <img src="https://tensorflow.google.cn/images/tf_logo_32px.png">  TensorFlow 1 version</a></td><td>  <a target="_blank" href="https://github.com/tensorflow/tensorflow/blob/r2.0/tensorflow/python/ops/clip_ops.py#L36-L93">    <img src="https://tensorflow.google.cn/images/GitHub-Mark-32px.png">    View source on GitHub  </a></td></tr></tbody></table>'
 b='Clips tensor values to a specified min and max.'
 c='<h3 id="aliases" is-upgraded="">Aliases:</h3>'
@@ -15,3 +16,95 @@ n='<h4 id="returns" is-upgraded="">Returns:</h4>'
 o='A clipped <code translate="no" dir="ltr">Tensor</code> or <code translate="no" dir="ltr">IndexedSlices</code>.'
 p='<h4 id="raises" is-upgraded="">Raises:<button role="button" class="devsite-heading-link button-flat material-icons" title="Copy link to this section"></button></h4>'
 q='<li><b><code translate="no" dir="ltr">ValueError</code></b>: If the clip tensors would trigger array broadcastingthat would make the returned tensor larger than the input.</li><li><b><code translate="no" dir="ltr">TypeError</code></b>: If dtype of the input is <code translate="no" dir="ltr">int32</code> and dtype ofthe <code translate="no" dir="ltr">clip_value_min\' or</code>clip_value_max<code translate="no" dir="ltr">is</code>float32`</li>'
+
+
+# 判断是否是href
+def is_href(element):
+	if re.match(r'^.*<a>',element):
+		return True
+	else:
+		return False
+def is_code(element):
+	if re.match(r'^.*<code>',element):
+		return True
+	else:
+		return False    
+
+# 判断所包围的标签还含有子标签
+def has_child(element):
+	pass
+def is_li(element):
+	if re.match(r'^.*<li>',element):
+		return True
+	else:
+		return False
+# 判断如果不是被包围的标签
+def is_no_wrap(element):
+	if re.match(r'^.*<(.*?)>.*$',element):
+		return False
+	else:
+		return True
+# li 可能还有其他子标签
+def parser_li(string):
+	temp_array=re.finditer(r'\<li\>(.*?)\<\/li\>',string)
+
+	text=''
+
+	# <li><a href="/api_docs/python/tf/clip_by_value"><code translate="no" dir="ltr">tf.compat.v1.clip_by_value</code></a></li>
+	# <li><a href="/api_docs/python/tf/clip_by_value"><code translate="no" dir="ltr">tf.compat.v2.clip_by_value</code></a></li>
+	for ele in temp_array:
+		pass
+
+# 将code 解析为``,
+# <code translate="no" dir="ltr">tf.compat.v2.clip_by_value</code>
+# => tf.compat.v2.clip_by_value
+def parser_code(string):
+	left=re.sub(r'<code(.*?)>','`',string.strip())
+	return re.sub(r'</code>','`',left)
+
+# 获取href地址url
+def get_href():
+	pass
+
+# 判断是哪种标签，todo，此时先判断code
+def check_what_element(elementString):
+	element_name=re.sub(r'<(.*?) .*$','\\1',elementString)
+	if element_name=='code':
+		return parser_code(elementString)
+	return ''
+
+	
+# 将a标签 <a href="/api_docs/python/tf/clip_by_value"><code translate="no" dir="ltr">tf.compat.v2.clip_by_value</code></a> 
+# 转为 [xx](xxx)
+def parser_a(elementString):
+	# todo 先检查a 标签里面包含其他东西
+	the_href_element= re.search(r'(href=")(.+?)(")',elementString)
+	the_href= re.sub(r'(href=")(.+?)(")','\\2',the_href_element.group()) # 获得a标签的地址
+
+	left =re.sub(r'<a(.*?)>','',elementString)
+	centerContent=re.sub(r'</a>','',left)
+	print("aaa:",centerContent)
+	return '['+check_what_element(centerContent)+']('+the_href+')'
+
+def parser_html(item):
+	text=''
+	if is_li(item):
+		text= parser_li(item)
+	return text
+
+# x =parser_html(d)
+
+# print(x)
+
+def tesc_code():
+	code='<code translate="no" dir="ltr">tf.compat.v2.clip_by_value</code>'
+	new_code=parser_code(code)
+	print("test code element:\n")
+	print(new_code)
+
+def test():
+	string='<a href="/api_docs/python/tf/clip_by_value"><code translate="no" dir="ltr">tf.compat.v1.clip_by_value</code></a>'
+	x =parser_a(string)
+	print(x)
+
+test()
