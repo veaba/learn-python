9899f5814ce78ea40121aa11b11b1d8bc7edf0ff
veaba <908662421@qq.com>
2019-12-18 18:20:06 +0800

    todo 增加标记算法描述

--- level.py
+++ level.py
@@ -21,6 +21,18 @@
 6. 右边永远是元素最大值
 7. 最元素个数为2个
 8. 左边的组合，并不是乱选出来的
+9. 步调可短可长，无法确定
+10. 左边对应的值，可比它大，也可以比它小
+
+# ***********排列组合问题***********
+11. 验证到最后，可以通过一个排列组合的问题来计算出来，但这个不够！
+12. 左边奇数，右边必定为偶数，反之亦然
+13. 左边+右边，必定为奇数,而值有重复的
+14. 最小奇数、最大奇数都是确定的
+
+# ***********标记算法***********
+15. [[][[]][]]
+16. [[][[]][[]]]
 """
 
 # all_array = []  # 可以根据left_array 算出来
@@ -28,7 +40,8 @@
 # left_array = [0, 1, 2, 3, 4] √
 # left_array = [0, 1, 2, 5, 7] √
 # left_array = [0, 1] √
-left_array = [0, 1, 3, 4, 5, 6, 11]
+# left_array = [0, 1, 3, 4, 5, 6, 11] √
+left_array = [0, 1, 3, 5, 7, 8, 9, 11]  # =>[15,2,4,6,14,13,10,12]
 
 
 def level(left):
@@ -55,24 +68,33 @@ def level(left):
         print(i, left[i])
         k = i  # 索引值
         v = left[i]  # 当前值
-        if k > 1:
-            # 算法一
-            if max_left_value < left_len_value:
-                right_value = max_value - (k * 2 - k)
-                right_array.insert(0, right_value)
-                print('算法一.A：', '(' + str(v) + '=>' + str(right_value) + ')')
-            # 算法二，还不对~
-            else:
-                right_value = all_len - (max_value - v)
-                right_array.insert(0, right_value)
-                print('算法二.B：', '(' + str(v) + '=>' + str(right_value) + ')')
-        elif k == 1:
-            spe_array = list(set(right_array_temp).difference(set(right_array)))
-            right_value = min(spe_array)
-            right_array.insert(0, right_value)
-            print('算法二.A：', '(' + str(v) + '=>' + str(right_value) + ')')
-        else:
-            print('算法三.A：', '(' + str(v) + '=>' + str(max_value) + ')')
+        # 最后一个
+        print('==>', k, left_len_value)
+        if k == left_len_value - 1:
+            right_array.insert(0, v + 1)
+        # 第一个
+        elif k == 0:
+            right_array.insert(0, max_value)
+
+        # # 第二个
+        # elif k == 1:
+        #     spe_array = list(set(right_array_temp).difference(set(right_array)))
+        #     right_value = min(spe_array)
+        #     right_array.insert(0, right_value)
+        # else:
+        #     pre_left_value = left[k + 1]  # 上一个索引值
+        #     step = pre_left_value - v  # 上个值减去当前的值
+        #     pre_right_value = right_array[0]  # 上个值对应左边的值
+        #     if pre_right_value + step in right_array or pre_right_value + step in left_array:
+        #         print('哈哈', k, pre_right_value - step)
+        #         right_array.insert(0, pre_right_value - step)
+        #     else:
+        #         print('哦哦', k, pre_right_value - step)
+        #         right_array.insert(0, pre_right_value + step)
+
+            # right_array.insert(0, '擦')
+    print('left:', left)
+    print('right:', right_array)
 
 
 level(left_array)
